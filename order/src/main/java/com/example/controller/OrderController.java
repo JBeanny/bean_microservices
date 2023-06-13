b11ccbe71b297759a59fdd5ef79d3ecd1e43fa78
jbeanny <yimsotharoth999@gmail.com>
2023-06-13 16:04:14 +0700

    added: product api to decrease product qty when order is placed

--- order/src/main/java/com/example/controller/OrderController.java
+++ order/src/main/java/com/example/controller/OrderController.java
@@ -2,6 +2,7 @@ package com.example.controller;
 
 import com.example.model.OrderModel;
 import com.example.service.OrderService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -9,6 +10,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/orders")
+@Slf4j
 public class OrderController {
 
     @Autowired
@@ -19,8 +21,13 @@ public class OrderController {
     }
 
     @PostMapping()
-    public void createOrder(@RequestBody OrderModel order) {
-        orderService.save(order);
+    public void createOrder(@RequestBody OrderModel order) throws Exception {
+        try{
+            orderService.save(order);
+            log.info("Placed order...");
+        }catch (Exception exp) {
+            log.info(exp.getMessage());
+        }
     }
 
 
--- order/src/main/java/com/example/service/OrderService.java
+++ order/src/main/java/com/example/service/OrderService.java
@@ -1,7 +1,11 @@
 package com.example.service;
 
 import com.example.model.OrderModel;
+import com.example.model.Product;
 import com.example.repository.OrderRepository;
+import com.example.util.Helper;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -20,13 +24,31 @@ public class OrderService {
         return orderRepository.findAll();
     }
 
-    public void save(OrderModel order) {
-        OrderModel orderToSave = OrderModel.builder()
-                .customerId(order.getCustomerId())
-                .productId(order.getProductId())
-                .orderDate(LocalDateTime.now())
-                .build();
-        log.info("Saving order: {}",order.getId());
-        orderRepository.save(orderToSave);
+    public void save(OrderModel order) throws Exception {
+        log.info("Checking product quantity ...");
+        String urlToRequest = "http://localhost:8084/api/v1/products/" + order.getProductId();
+        String response = new Helper().callApiToRetrieve(urlToRequest);
+        ObjectMapper objectMapper = new ObjectMapper();
+        Product product = objectMapper.readValue(response,Product.class);
+
+        if(product.getProductAmount() >= 1) {
+            log.info("Product quantity sufficient ...");
+            log.info("Grabbing product ...");
+            // decrease product amount by 1
+            new Helper().callApiToPatch(urlToRequest);
+            log.info("Product is grabbed ...");
+
+            OrderModel orderToSave = OrderModel.builder()
+                    .customerId(order.getCustomerId())
+                    .productId(order.getProductId())
+                    .orderDate(LocalDateTime.now())
+                    .build();
+            log.info("Saving order ...");
+            orderRepository.save(orderToSave);
+        }else{
+            log.info("Not enough amount ...");
+            throw new Exception("Product amount insufficient");
+        }
+
     }
 }
