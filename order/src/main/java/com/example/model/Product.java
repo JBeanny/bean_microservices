66f222e05df83f9ce94d07f8158941f5df71180c
jbeanny <yimsotharoth999@gmail.com>
2023-06-13 16:05:13 +0700

    added: product model in Order Service

---
+++ order/src/main/java/com/example/model/Product.java
@@ -0,0 +1,18 @@
+package com.example.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Product {
+    private Long id;
+    private String productName;
+    private String productBrand;
+    private Integer productAmount;
+    private Double productPrice;
+}
