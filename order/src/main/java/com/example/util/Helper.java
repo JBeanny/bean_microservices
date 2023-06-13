9b06ceb896d48534be242bb19e38c98d3ffa1746
jbeanny <yimsotharoth999@gmail.com>
2023-06-13 16:04:58 +0700

    created: API call function for retrieve and update for product API

---
+++ order/src/main/java/com/example/util/Helper.java
@@ -0,0 +1,25 @@
+package com.example.util;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import java.time.Duration;
+
+public class Helper {
+    WebClient webClient = WebClient.create();
+    public String callApiToRetrieve(String apiUrl){
+        return webClient.get()
+                .uri(apiUrl)
+                .retrieve()
+                .bodyToMono(String.class)
+                .block();
+    }
+
+    public void callApiToPatch(String apiUrl){
+         webClient.patch()
+                .uri(apiUrl)
+                 .retrieve()
+                 .bodyToMono(String.class)
+                 .subscribe(System.out::println);
+    }
+}
